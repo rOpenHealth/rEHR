b363329557c9c700f4ebd8ef2d7733d8ad57af8b
David Springate <daspringate@gmail.com>
2014-02-07 12:10:01 +0000

    Fixed date imports and year of birth field

--- R/cprd_import.R
+++ R/cprd_import.R
@@ -15,6 +15,7 @@ read_zip <- function(file, ...) {
 
 
 #' Wrapper for dbconnect
+#' Connects to a SQLite database or creates one if it does not already exist
 #' @param dbname a name for the new database
 #' @export
 database <- function(dbname){
@@ -26,8 +27,11 @@ database <- function(dbname){
 #' @param db a database connection object
 #' @param files a character vector of filenames to files to be imported
 #' @param table_name a name for the table to import to
+#' @param dateformat the format that dates are stored in the CPRD data.  If this is wrong it won't break but all dates are likely to be NA
+#' @param yob_origin value to add yob values to to get actual year of birth (Generally 1800)
 #' @export
-add_to_database <- function(db, files, table_name){
+add_to_database <- function(db, files, table_name, dateformat = "%d/%m/%Y", yob_origin = 1800){
+    date_fields <- c("eventdate", "sysdate", "lcd", "uts", "frd", "crd", "tod", "deathdate")
     for(f in files){
         if(str_detect(f, "zip$")){
             message(sprintf("Unzipping %s...", f), appendLF = FALSE)
@@ -36,6 +40,14 @@ add_to_database <- function(db, files, table_name){
             message(sprintf("Reading %s...", f), appendLF = FALSE)
             dat <- read.delim(f)
         }
+        f_dates <- intersect(names(dat), date_fields)
+        if(length(f_dates)){
+            message(" Converting date formats...", appendLF = FALSE)
+            for(column in f_dates){
+                dat[[column]] <- as.character(as.Date(dat[[column]], format = dateformat))
+            }
+        }
+        if("yob" %in% names(dat)) dat$yob <- dat$yob + yob_origin
         message(sprintf(" Importing to table '%s'...", table_name))
         if(dbExistsTable(db, table_name)){
             dbWriteTable(conn = db, 
@@ -56,11 +68,14 @@ add_to_database <- function(db, files, table_name){
 #' @param db a database connection
 #' @param data_dir the directory containing the CPRD cohort data
 #' @param filetypes character vector of filetypes to be imported
+#' @param dateformat the format that dates are stored in the CPRD data.  If this is wrong it won't break but all dates are likely to be NA
+#' @param yob_origin value to add yob values to to get actual year of birth (Generally 1800)
 #' @export
 import_CPRD_data <- function(db, data_dir,
                              filetypes = c("Additional", "Clinical", "Consultation", 
                                            "Immunisation", "Patient", "Practice", 
-                                           "Referral", "Staff", "Test", "Therapy")){
+                                           "Referral", "Staff", "Test", "Therapy"),
+                             dateformat = "%d/%m/%Y", yob_origin = 1800){
     all_files <- list.files(data_dir)
     for(filetype in filetypes){
         current <- all_files[str_detect(all_files, paste("PET", filetype, sep = "_"))]
@@ -68,7 +83,8 @@ import_CPRD_data <- function(db, data_dir,
         if(!length(current)){
             message(sprintf("No %s files to import.", filetype))
         } else {
-            add_to_database(db, files = current, table_name = filetype)
+            add_to_database(db, files = current, table_name = filetype, 
+                            dateformat = dateformat, yob_origin = yob_origin)
         }
     }
 }
