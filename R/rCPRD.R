b065b8385b68fb79f94b04426325b75d72da2f7d
David Springate <daspringate@gmail.com>
2014-03-25 09:31:24 +0000

    dplyr and sqldf as dependencies

--- R/rCPRD.R
+++ R/rCPRD.R
@@ -3,10 +3,6 @@
 #' @docType package
 #' @name rCPRD
 #' @import assertthat
-#' @import sqldf
-#' @import DBI
-#' @import RSQLite
 #' @import stringr
-#' @importFrom dplyr translate_sql_q
 #' @importFrom foreign write.dta
 NULL
