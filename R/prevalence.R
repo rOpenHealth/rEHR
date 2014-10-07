7ca5c6589c6482ea1e2095674769dac4ffaaed5e
David Springate <daspringate@gmail.com>
2014-10-07 12:29:09 +0100

    Improved prevalence fns to cope with any aggregate vars

--- R/prevalence.R
+++ R/prevalence.R
@@ -2,28 +2,35 @@
 #' 
 #' e.g. Run on the output of a call to select_by_year
 #' 
-#' Outputs totals by practice and/or by patient
+#' Outputs totals by aggregated by a time variable and other variables
 #' 
 #' @export
 #' 
 #' @param dat a dataframe
-#' @param included_totals character vector describing which aggregates should be included c("patient", "practice")
+#' @param included_totals character vector describing which aggregates should be included e.g. c("year", "practid")
+#' @param time_var name of the variable determining timepoints
+#' @param patient_var name of the variable determining patient id for counting
 #' @param original_data logical should the original data set be included in the output
-#' @param year_var name of the variable determining year
-#' @param patient_var name of the variable determining patient id
-#' @param practice_var name of the variable determining practice id
 #' @return list of aggregates and/or the original data
-prev_totals <- function(dat, included_totals = c("patient", "practice"), original_data = TRUE,
-                        year_var = "year", patient_var = "patid", practice_var = "practid"){
-    out <- list()
-    if("patient" %in% included_totals) out$total_counts = aggregate(dat[[patient_var]], 
-                                                                   by = list(year = dat[[year_var]]), 
-                                                                   FUN = length)
-    if("practice" %in% included_totals) out$practice_counts = aggregate(dat[[patient_var]], 
-                                                                        by = list(year = dat[[year_var]], 
-                                                                        practice = dat[[practice_var]]), 
-                                                                        FUN = length)
-    if(original_data) out$patients <- dat
+prev_totals <- function(dat, included_totals = c("year", "practid"), 
+                        time_var = "year", patient_var = "patid", original_data = TRUE){
+    out <- lapply(included_totals, function(x){
+        if(x == time_var){
+            agg_list <- list(dat[[time_var]])
+            names(agg_list) <- time_var
+            aggregate(dat[[patient_var]], 
+                      by = list(year = dat[[time_var]]), 
+                      FUN = length)
+        } else {
+            agg_list <- list(dat[[time_var]], dat[[x]])
+            names(agg_list) <- c(time_var, x)
+            aggregate(dat[[patient_var]], 
+                      by = agg_list,
+                      FUN = length)    
+        }
+    })
+    names(out) <- paste(included_totals, "counts", sep = "_")
+    if(original_data) out$data <- dat
     out
 }
 
