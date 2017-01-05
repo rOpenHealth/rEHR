c7cf07adbd07f7449b8007fca8f2bbb677d0bdb8
Rosa <rparisi09@gmail.com>
2017-01-05 10:42:31 +0000

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,13 @@
-.onLoad <- function(libname, pkgname){
-    set_CPRD()
-}
-
+   .onLoad <- function(libname, pkgname){
+     set_CPRD()
+     
+     # CRAN Note avoidance
+     if(getRversion() >= "2.15.1") 
+       utils::globalVariables(
+         # Variables getting the Note: "no visible binding for global variable"
+         c("index_diff","prev_denom","followup","denominator","numerator","incid_denom",
+           "eventdate","practid","events","birthday","tod","toreason","days","practid"
+         )
+       )
+     invisible()
+   }
